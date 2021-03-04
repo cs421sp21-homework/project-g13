ce0851464d72a2b903ba16f6458c3ba29487ca2b
Abdullah Yousuf <23.abdullah.y@gmail.com>
2021-03-04 18:45:04 +0300

    Worked on displaying restaurants, updated settings

--- code/frontend/src/main/resources/public/js/card.js
+++ code/frontend/src/main/resources/public/js/card.js
@@ -12,10 +12,23 @@ class Card extends React.Component {
     render() {
         return (
             <div class="card-body">
-                <div class="restaurant-image"></div>
-                <h1>{this.state.restaurant_name}</h1>
-                <h2>{this.state.price_range}</h2>
-                <h2>{this.state.cuisine}</h2>
+                <div class="restaurant-image">
+
+                </div>
+                <div class="restaurant-info">
+                    <h1 class="display-inline font-helvetica restaurant-name">{this.state.restaurant_name}</h1>
+                    <h2 class="display-inline font-helvetica restaurant-price-range">{this.state.price_range}</h2>
+                    <h2 class="font-helvetica restaurant-cuisine">{this.state.cuisine}</h2>
+                </div>
+
+                <div class="like-dislike-container">
+                    <button class="like-dislike-button float-left red">
+                        <i class="fas fa-heart"></i>
+                    </button>
+                    <button class="like-dislike-button float-right">
+                        <i className="fas fa-times"></i>
+                    </button>
+                </div>
             </div>
         );
     }
