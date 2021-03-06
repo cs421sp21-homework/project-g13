49be5db9fe1e7d0aa528488eaebbb07b785aa7c1
Your Name <ecsnz3@gmail.com>
2021-03-06 01:40:44 -0500

    Rough draft of getting data with axios

--- code/frontend/src/Api.js
+++ code/frontend/src/Api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 async function getRestaurants() {
-    const response = axios.get('http://chicken-tinder-13-backend.herokuapp.com/hello-world-onlybackend');
+    const response = axios.get('https://chicken-tinder-13-backend.herokuapp.com/hello-world-onlybackend');
     return response;
 }
 
