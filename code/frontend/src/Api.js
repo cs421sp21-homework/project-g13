beb510c1acd3b2c71d5d4a7343ee35ec8fc66efc
ericsaenz <ecsnz3@gmail.com>
2021-03-06 13:49:17 -0500

    Cleanup

--- code/frontend/src/Api.js
+++ code/frontend/src/Api.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 async function getRestaurants(location) {
-    console.log(location);
     try {
         const response = await axios.get(
             `https://chicken-tinder-13-backend.herokuapp.com/search?query=${location}`);
