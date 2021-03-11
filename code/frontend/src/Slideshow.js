212c0c370465f18601caf4550a1be0857ea1a0b8
= <nathanieleisenberg@gmail.com>
2021-03-11 11:20:15 -0500

    updated image sizes

--- code/frontend/src/Slideshow.js
+++ code/frontend/src/Slideshow.js
@@ -1,49 +1,54 @@
-import React, { Component } from 'react';
-import Carousel from 'react-bootstrap/Carousel';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from "react";
+import Carousel from "react-bootstrap/Carousel";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class Slideshow extends Component {
-    render() {
+  render() {
+    const { photos, reviews } = this.props;
 
-        const {photos, reviews} = this.props;
+    return (
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            width="10"
+            height="500"
+            src={photos[0]}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <p>{reviews[0]["text"]}</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            width="10"
+            height="500"
+            src={photos[1]}
+            alt="Second slide"
+          />
 
-        return (
-            <Carousel>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={photos[0]}
-                        alt="First slide"
-                    />
-                    <Carousel.Caption>
-                        <p>{reviews[0]['text']}</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={photos[1]}
-                        alt="Second slide"
-                    />
+          <Carousel.Caption>
+            <p>{reviews[1]["text"]}</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            width="10"
+            height="500"
+            src={photos[2]}
+            alt="Third slide"
+          />
 
-                    <Carousel.Caption>
-                        <p>{reviews[1]['text']}</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src={photos[2]}
-                        alt="Third slide"
-                    />
-
-                    <Carousel.Caption>
-                        <p>{reviews[2]['text']}</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
-        )
-    }
+          <Carousel.Caption>
+            <p>{reviews[2]["text"]}</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    );
+  }
 }
 
 export default Slideshow;
