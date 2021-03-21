96970dc3b6ee16a9966847f035212134cc7cd135
Abdullah Yousuf <23.abdullah.y@gmail.com>
2021-03-21 20:56:52 +0300

    updated cors settings

--- code/socketio/Groups.js
+++ code/socketio/Groups.js
@@ -8,10 +8,13 @@ const socketio = require("socket.io");
 
 const server = require("http").createServer();
 const options = {
-  cors: {
-    origin: "*",
-    credentials: true,
-  },
+  allowUpgrades: true,
+  transports: [ 'polling', 'websocket' ],
+  pingTimeout: 9000,
+  pingInterval: 3000,
+  cookie: 'mycookie',
+  httpCompression: true,
+  cors: '*:*',
 };
 
 const io = require("socket.io")(server, options);
