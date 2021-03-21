602bcdc0ca947e03d978949089a897f147e39d72
Siqi Cao <scao16@jhu.edu>
2021-03-20 20:10:46 -0400

    create login

---
+++ code/frontend/src/components/Login/SubmitButton.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class SubmitButton extends React.Component {
+
+    render() {
+        return (
+            <div className="submitButton">
+                asdsase
+            </div>
+        );
+    }
+}
+
+export default SubmitButton;
