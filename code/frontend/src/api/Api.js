9528b803dd3c6a1bf0f15f40154d104be0440fa6
ericsaenz <ecsnz3@gmail.com>
2021-03-25 22:05:09 -0400

    Update Api.js

--- code/frontend/src/api/Api.js
+++ code/frontend/src/api/Api.js
@@ -19,4 +19,32 @@ async function getRestaurants(location, radius) {
     }
 }
 
-export {getRestaurants};
+async function postGroup() {
+    const response = await axios.post(`${BACKEND_URL}/api/groups`);
+    return response.data;
+}
+
+async function postUser(uname, pwd, loc, groupid) {
+    const data = JSON.stringify({
+        "userName": uname,
+        "pword": pwd,
+        "loc": loc,
+        "group_ID": groupid
+    });
+
+    const config = {
+        method: 'post',
+        url: 'https://chicken-tinder-13-backend.herokuapp.com/api/users',
+        data : data
+    };
+
+    const response = await axios(config);
+    return response.data;
+}
+
+async function getGroupMembers(groupid) {
+    const response = await(axios.get(`${BACKEND_URL}/api/groups/${groupid}`));
+    return response.data;
+}
+
+export {getRestaurants, postGroup, postUser, getGroupMembers};
