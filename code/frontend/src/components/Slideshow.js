97388b39efcacb21ea5ba78d249d94378274145a
Shanelle Cao <shanelle.cao@Shanelles-MacBook-Pro.local>
2021-04-03 10:52:46 -0700

    updated website style

--- code/frontend/src/components/Slideshow.js
+++ code/frontend/src/components/Slideshow.js
@@ -14,12 +14,6 @@ class Slideshow extends Component {
             src={photos[0]}
             alt="First slide"
           />
-
-          <Carousel.Caption>
-            <p style={{ color: "black", display: "block" }}>
-              {reviews[0]["text"]}
-            </p>
-          </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
@@ -27,12 +21,6 @@ class Slideshow extends Component {
             src={photos[1]}
             alt="Second slide"
           />
-
-          <Carousel.Caption>
-            <p style={{ color: "black", display: "block" }}>
-              {reviews[1]["text"]}
-            </p>
-          </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
@@ -40,12 +28,6 @@ class Slideshow extends Component {
             src={photos[2]}
             alt="Third slide"
           />
-
-          <Carousel.Caption>
-            <p style={{ color: "black", display: "block" }}>
-              {reviews[2]["text"]}
-            </p>
-          </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
     );
